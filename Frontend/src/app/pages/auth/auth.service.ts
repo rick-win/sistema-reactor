a01486f537d88bb5389c712fba9da69e18a4f709
David Idrobo <davididrobob@gmail.com>
2021-06-21 16:20:59 -0500

    Summ

--- Frontend/src/app/pages/auth/auth.service.ts
+++ Frontend/src/app/pages/auth/auth.service.ts
@@ -5,28 +5,43 @@ import { UserResponse, User } from '../../shared/models/user.interface';
 import { environment } from 'src/environments/environment';
 
 import {catchError, map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
+
+const helper = new JwtHelperService();
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.checkToken() ;
+   }
 
   login(authData:User):Observable<UserResponse | void>{
     return this.http.post<UserResponse>(`${environment.API_URL}/auth/login`, authData)
     .pipe(
       map((res: UserResponse)=>{
-        console.log('Res =>', res)
-        //saveToken()
+        this.saveToken(res.token);
       }),
       catchError((err)=>this.handlerError(err))
     );
     
   }
-  logout():void{}
-  private readeToken():void{}
-  private saveToken(): void{}
+  logout():void{
+    localStorage.removeItem('token');
+    //set userIslogged = false
+  }
+  private checkToken():void{
+    const userToken: string | any = localStorage.getItem('token') ;
+       
+    const isExpired = helper.isTokenExpired(userToken);
+    console.log('isExpired =>', isExpired);
+    // set userIslogged = isExpired
+
+  }
+  private saveToken(token: string): void{
+    localStorage.setItem('token', token);
+  }
   
   private handlerError(err:any): Observable<never>{
     let errorMesage = "Se produjo un error al recuperar datos";
