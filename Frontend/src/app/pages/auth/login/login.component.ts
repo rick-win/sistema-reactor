05dfa6f9de37cc6aea28ef74b63164afccddc508
David Idrobo <davididrobob@gmail.com>
2021-06-23 02:13:12 -0500

    Desuscribirse de un observable
    Permite liberar uso de memoria de la app

--- Frontend/src/app/pages/auth/login/login.component.ts
+++ Frontend/src/app/pages/auth/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import{FormBuilder} from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -8,7 +9,8 @@ import { AuthService } from '../auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  private suscripcions: Subscription = new Subscription;
   loginForm = this.fb.group({
     username: [''],
     password: [''],
@@ -18,7 +20,7 @@ export class LoginComponent implements OnInit {
     private fb:FormBuilder,
     private router: Router
     ) { }
-
+    
   ngOnInit(): void {
    /* const userData = {
       username: 'davidIdrobo',
@@ -27,12 +29,18 @@ export class LoginComponent implements OnInit {
     this.authSvc.login(userData).subscribe((user) => console.log('Login'))*/
   }
 
+  ngOnDestroy(): void {
+    this.suscripcions.unsubscribe();
+  }
+
   onLogin():void{
     const formvalue = this.loginForm.value;
-    this.authSvc.login(formvalue).subscribe( res=>{
-      if(res){
-        this.router.navigate(['']);
-      }
+    this.suscripcions.add(
+      this.authSvc.login(formvalue).subscribe( res=>{
+        if(res){
+          this.router.navigate(['']);
+        }
     })
+    );
   }
 }
--- Frontend/src/app/shared/components/header/header.component.ts
+++ Frontend/src/app/shared/components/header/header.component.ts
@@ -1,23 +1,35 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { AuthService } from '../../../pages/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   isAdmin = false;
   islogged = false; 
 
+
+  private suscripcion: Subscription = new Subscription;
+
   @Output() toggleSidenav= new EventEmitter<void>();
 
   constructor(private authSvc: AuthService) { }
-
+  
   ngOnInit(): void {
-    this.authSvc.islogged.subscribe((res)=>(this.islogged = res));
+    this.suscripcion.add(
+      this.authSvc.islogged.subscribe((res)=>(this.islogged = res))
+    );
+    
   }
+  
+  ngOnDestroy(): void {
+    this.suscripcion.unsubscribe();
+  }
+
 
   onToggleSidenav(): void{
     this.toggleSidenav.emit();
